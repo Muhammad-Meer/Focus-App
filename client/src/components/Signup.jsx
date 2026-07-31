18d8ab86c0f12e4c008251a1b4f8b415b31d28b5
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-31 00:04:45 -0700

    newcoc

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -9,6 +9,7 @@ export default function Signup() {
   const navigate = useNavigate();
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
