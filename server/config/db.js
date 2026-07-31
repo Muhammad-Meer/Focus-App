9fc7fd60f996e4e2e52936950ad7f5307aee5be8
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-30 23:41:12 -0700

    newcodeee

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dns = require("dns");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const connectDB = require('./config/db')
 
 const authRoutes = require("./routes/auth.route");
 
 const app = express();
+connectDB()
 
 // Middleware
 app.use(cors());
@@ -17,10 +15,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
