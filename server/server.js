2444b39700401da2f4a25048ad04287968f6769c
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-31 23:13:04 -0700

    Update focus backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const focusRoutes = require("./routes/focus.route");
 const connectDB = require('./config/db')
 
 const authRoutes = require("./routes/auth.route");
@@ -13,6 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/focus", focusRoutes);
 
 
 const PORT = process.env.PORT || 5000;
